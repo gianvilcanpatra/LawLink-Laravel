292f5e11109d0b05dd9c3a91ec57a76062017a93
naufalfad <naufalfad29@gmail.com>
2024-05-24 17:25:17 +0700

    Autentikasi per-akun

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\LawyerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,8 +24,13 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/login', [AuthController::class, 'login']);
 // Route::middleware('auth:api')->get('/lawyers', [UserController::class, 'index']);
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::get('/lawyers', [UserController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
